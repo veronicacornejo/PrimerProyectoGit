f34fb5b9a6da5920ab955a7756b77090bcaac074
Verito <Verito@PCVERO>
2021-09-21 21:10:43 -0400

    Primer commit

---
+++ PirmerProyecto/src/PrimerModulo/Principal.java
@@ -0,0 +1,41 @@
+package PrimerModulo;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Instrucciones
+		//Primero debes 
+		//procurar
+		/*Hola este es un comentario
+		 * grande*/
+		
+		System.out.println(2+2);
+		int nutella;
+		nutella=22013440;
+		System.out.println(nutella);
+		
+		//21/09/2021
+		float flotante=5.6f; //
+		double doble=4; //mayor precision
+		double doble2=3.1415;
+		String texto="El numero telefonico es: "; //cadena
+		
+		System.out.println(nutella);
+		System.out.println(flotante);
+		System.out.println(doble);
+		System.out.println(texto+nutella);
+		
+		boolean valor=true;//puede ser true oo false 
+		valor=false;
+		double resultado=doble/doble2;
+		float resultadoF=(float)(doble/doble2);
+		float resultadoF1=(float)doble/(float)doble2;
+		int num=302;
+		int num2=400;
+		int res=num*num2;
+		float resf=num*num2;
+		double resd=num*num2;
+	}
+
+}
